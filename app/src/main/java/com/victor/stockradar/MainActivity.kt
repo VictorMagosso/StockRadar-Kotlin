416f88a6fc981f3ee97855fc2b08429212f30019
Victor Magosso <victor_magosso@hotmail.com>
2021-06-23 22:59:58 -0300

    funcionando

--- app/src/main/java/com/victor/stockradar/MainActivity.kt
+++ app/src/main/java/com/victor/stockradar/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.victor.stockradar
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -10,19 +12,32 @@ import com.victor.stockradar.viewmodel.factory.StockViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: StockViewModel
-    val stockCode = "bidi4"
+    private var stockCode = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
         val stock = findViewById<TextView>(R.id.txtStock)
+        val stockPrice = findViewById<TextView>(R.id.txtPrice)
+        val stockDescription = findViewById<TextView>(R.id.txtDescription)
+        val stockCompanyName = findViewById<TextView>(R.id.txtCompanyName)
+        val editStock = findViewById<EditText>(R.id.editStockCode)
+        val btnChooseStock = findViewById<Button>(R.id.btnChooseStock)
 
         val viewModelFactory = StockViewModelFactory(StockRepository())
         viewModel = ViewModelProvider(this, viewModelFactory).get(StockViewModel::class.java)
 
-        viewModel.getFund(stockCode)
-        viewModel.stockResponse.observe(this, { res ->
-            stock.text = res.body()?.results!![stockCode.uppercase()]?.stockName
-        })
+        btnChooseStock.setOnClickListener {
+            stockCode = editStock.text.toString().trim().lowercase()
+            viewModel.getFund(stockCode)
+            viewModel.stockResponse.observe(this, { res ->
+                stock.text = res.body()?.results!![stockCode.uppercase()]?.symbol
+                stockPrice.text = "R\$ ${res.body()?.results!![stockCode.uppercase()]?.price?.replace(".", ",")}"
+                stockCompanyName.text = res.body()?.results!![stockCode.uppercase()]?.company_name
+                stockDescription.text = res.body()?.results!![stockCode.uppercase()]?.description
+            })
+        }
     }
 }
--- app/src/main/java/com/victor/stockradar/model/Stock.kt
+++ app/src/main/java/com/victor/stockradar/model/Stock.kt
@@ -4,7 +4,14 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class Stock(
-    @SerializedName("name")
     @Expose
-    val stockName: String
-)
+    val name: String,
+    @Expose
+    val company_name: String,
+    @Expose
+    val description: String,
+    @Expose
+    val price: String,
+    @Expose
+    val symbol: String,
+    )
