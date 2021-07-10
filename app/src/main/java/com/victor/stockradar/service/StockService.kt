2dca615b7bacf91082595770dce95b5e1bbad9a2
Victor Magosso <victor_magosso@hotmail.com>
2021-07-10 16:14:43 -0300

    refatorando API_KEY

--- app/src/main/java/com/victor/stockradar/service/StockService.kt
+++ app/src/main/java/com/victor/stockradar/service/StockService.kt
@@ -1,5 +1,6 @@
 package com.victor.stockradar.service
 
+import com.victor.stockradar.BuildConfig
 import com.victor.stockradar.constants.Constants
 import com.victor.stockradar.model.ApiResponse
 import com.victor.stockradar.model.Stock
@@ -8,6 +9,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface StockService {
-    @GET("stock_price?key=${Constants.API_KEY}&symbol=symbol")
+    @GET("stock_price?key=${BuildConfig.API_KEY}&symbol=symbol")
     suspend fun getStock(@Query("symbol") stockCode: String) : Response<ApiResponse>
 }
